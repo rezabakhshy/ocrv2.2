426d38b81fe918c964d6c7ab4fbb0110d398e28f
rezabakhshy <87357982+rezabakhshy@users.noreply.github.com>
2021-11-26 19:19:50 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 from typing import Text
 from pyrogram import Client,filters
 import os
-app = Client("my_accound",api_id=12721742,api_hash="2a81674bd5e1ccbaed8c07f898d614ca")
-
-@app.on_message(filters.text)
+app = Client("my_accound",api_id=13893053,api_hash="f586d92837b0f6eebcaa3e392397f47c")
+last_id=str()
+message_id=str()
+@app.on_message(filters.me)
 def ocr(client, message):
-    
+    global last_id
+    global message_id
+    message_id=message.message_id
+    last_id = message.chat.id
+    text=message.text
     text2=text.split()[0]
     if text2=="!ocr":
         text=message.reply_to_message.photo.file_id
@@ -13,8 +18,8 @@ def ocr(client, message):
         client.send_photo("ocr_prov_bot","downloads/test.jpg")
         os.remove("downloads/test.jpg")
        
-    elif message.reply_to_message:
-        text=message.text
-        client.send_message("rezabz2",text)
-
+@app.on_message(filters.chat(2143804610))
+def forward(client,message):
+    text=message.text
+    client.send_message(chat_id=last_id,text=text,reply_to_message_id=message_id)
 app.run()
